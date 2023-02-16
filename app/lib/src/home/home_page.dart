dd747c7f81a10afd41df9588f9794941b4a7b3c0
Célio Costa <celiorscosta@hotmail.com>
2023-02-16 17:13:08 -0300

    floating button.

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -11,7 +11,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('LISTINHA')),
+      appBar: AppBar(
+        title: const Text('LISTINHA'),
+        actions: [
+          CircleAvatar(
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Colors.white,
+            child: const Text('A'),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.edit),
+        label: const Text('Nova Lista'),
+        onPressed: () {},
+      ),
     );
   }
 }
