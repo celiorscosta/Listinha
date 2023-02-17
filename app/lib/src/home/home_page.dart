b48f67dff52ef7930374fcc4f04ad634b6e8cb12
Célio Costa <celiorscosta@hotmail.com>
2023-02-17 09:16:35 -0300

    SegmentedButton adicionado e separado widget para image button.

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listinha/src/shared/widgets/user_image_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,11 +14,39 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('LISTINHA'),
-        actions: [
-          CircleAvatar(
-            backgroundColor: Theme.of(context).primaryColor,
-            foregroundColor: Colors.white,
-            child: const Text('A'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 8),
+            child: UserImageButton(),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: SegmentedButton<int>(
+              segments: const [
+                ButtonSegment(
+                  value: 0,
+                  label: Text('Todos'),
+                ),
+                ButtonSegment(
+                  value: 1,
+                  label: Text('Pendentes'),
+                ),
+                ButtonSegment(
+                  value: 2,
+                  label: Text('Concluídas'),
+                ),
+                ButtonSegment(
+                  value: 3,
+                  label: Text('Desativadas'),
+                ),
+              ],
+              selected: const {0},
+              onSelectionChanged: (values) {},
+            ),
           )
         ],
       ),
