bc65883c5e2801350a89b62e6d446038f0f39743
Célio Costa <celiorscosta@hotmail.com>
2023-02-21 09:20:32 -0300

    Refatorações da aula 06

--- app/lib/src/app_module.dart
+++ app/lib/src/app_module.dart
@@ -1,7 +1,7 @@
 import 'dart:js';
 
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:listinha/src/home/widgets/home_module.dart';
+import 'package:listinha/src/home/home_module.dart';
 
 import 'configuration/configuration_page.dart';
 
