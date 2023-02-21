48ebb779e197db3d917154590edc0399fcd51a5c
Célio Costa <celiorscosta@hotmail.com>
2023-02-21 13:33:25 -0300

    Layout da tela de Configurações

--- app/lib/src/configuration/configuration_page.dart
+++ app/lib/src/configuration/configuration_page.dart
@@ -14,6 +14,55 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
       appBar: AppBar(
         title: const Text('LISTINHA'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Tema',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 10),
+            RadioListTile(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.system,
+              title: Text(
+                'Sistema',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              onChanged: (mode) {},
+            ),
+            RadioListTile(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.light,
+              title: const Text('Claro'),
+              onChanged: (mode) {},
+            ),
+            RadioListTile(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.light,
+              title: const Text('Escuro'),
+              onChanged: (mode) {},
+            ),
+            const SizedBox(height: 20),
+            const Text('Controle de Dados'),
+            const SizedBox(height: 10),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text(
+                'Apagar cache e reiniciar app',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
