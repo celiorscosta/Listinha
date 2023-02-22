8183bd92c8a35dd4011515c0625ee27b0916360c
Célio Costa <celiorscosta@hotmail.com>
2023-02-22 12:34:33 -0300

    melhorias e formatação de data.

--- app/lib/src/shared/stores/app_store.dart
+++ app/lib/src/shared/stores/app_store.dart
@@ -2,13 +2,25 @@ import 'package:flutter/material.dart';
 
 class AppStore {
   final themeMode = ValueNotifier(ThemeMode.system);
+
   void changeThemeMode(ThemeMode? mode) {
     if (mode != null) {
       themeMode.value = mode;
     }
   }
 
+  final syncDate = ValueNotifier<DateTime?>(null);
+
+  void setSyncDate(DateTime date) {
+    syncDate.value = date;
+    save();
+  }
+
   void save() {
+    //TODO: Salvar os dados na base local.
+  }
+
+  void init() {
     //TODO: Salvar os dados na base local.
   }
 }
